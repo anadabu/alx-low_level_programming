01c4fa9c0ecb75fc918e19239668ad891787b7c7
anadabu <ajgocho@gmail.com>
2023-07-14 02:40:24 +0000

    corrected advamced task2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
+ * main - Entry point
  *
- * Return: 1 this time around
+ * Return: 1 (failure)
  */
 int main(void)
 {
-	write(1, "with proper grammar, but the outcome is a piece of art,\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
