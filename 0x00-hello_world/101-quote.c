72b87c737f5458ecff5c3e2417e5438b593bbe92
anadabu <ajgocho@gmail.com>
2023-07-14 02:18:28 +0000

    added advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ *
+ * Return: 1 this time around
+ */
+int main(void)
+{
+	write(1, "with proper grammar, but the outcome is a piece of art,\n");
+	return (1);
+}
