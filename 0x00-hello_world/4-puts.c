1f471125610f84f8f2f836771d95e97fee7d4c18
anadabu <ajgocho@gmail.com>
2023-07-14 01:21:18 +0000

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a line using the puts function
+ *
+ * Return: 0 as always
+ */
+int main (void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
