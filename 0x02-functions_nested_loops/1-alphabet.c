a61d7f2f61f53b4d692972e558ecc1d7a05fe138
anadabu <ajgocho@gmail.com>
2023-07-19 22:42:08 +0000

    added file2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Produces the alphabets
+ *
+ * Return: void for today
+ */
+
+void print_alphabet (void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: 0 as usual
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
