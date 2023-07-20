972101a328c3b4bebfa00b152e2658351d08dece
anadabu <ajgocho@gmail.com>
2023-07-20 00:26:00 +0000

    corrected file11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two values
+ *
+ * @n1: The first umber
+ *
+ * @n2: The second number
+ *
+ * Return: Result of addition
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
