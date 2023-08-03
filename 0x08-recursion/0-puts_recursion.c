e093167d8c5887aab3a5a6e13bf1fa5e763d77ed
anadabu <ajgocho@gmail.com>
2023-08-03 03:05:58 +0000

    added file1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a strign followed by a new line
+ * @s: strign to be printed
+ * Return: nothing at all
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
