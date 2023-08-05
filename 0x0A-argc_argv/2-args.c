95e805f80f9d43f4f170f443747dd20e97c5c5d8
anadabu <ajgocho@gmail.com>
2023-08-05 01:34:28 +0000

    added file3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lists of arguemnts
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
+
