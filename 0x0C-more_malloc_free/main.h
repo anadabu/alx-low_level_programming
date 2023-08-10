1f2aa23f30037aa214acc005d3605cf193d7de64
anadabu <ajgocho@gmail.com>
2023-08-10 19:17:38 +0000

    added file2. _calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
