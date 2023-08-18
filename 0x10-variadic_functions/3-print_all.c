695fbec78b4b0248df729826a6e2b7b72c5eb116
anadabu <ajgocho@gmail.com>
2023-08-17 18:53:59 -0700

    corrected file 3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,43 +8,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0;
-	char c;
-	char *s;
-	int int_value;
-	float float_value;
+	int i;
+	int flag;
+	char *str;
+	va_list a_list;
 
-	va_start(args, format);
-	while (format[i] != '\0')
+	va_start(a_list, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
-				c = va_arg(args, int);
-				printf("%c", c);
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'i':
-				int_value = va_arg(args, int);
-				printf("%d", int_value);
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'f':
-				float_value = (float)va_arg(args, double);
-				printf("%f", float_value);
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
 				break;
 			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					printf("(nil)");
-				else
-					printf("%s", s);
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i'
-		|| format[i] == 'f' || format[i] == 's'))
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(a_list);
 }
